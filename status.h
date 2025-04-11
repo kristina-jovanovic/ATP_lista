621548490a3326aaec0bb9e7cc548ac09c7c69fd
kristina-jovanovic <jovanovick25@gmail.com>
2025-04-11 19:27:33 +0200

    dodat status.h

---
+++ status.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "defs.h"
+
+// tipovi
+typedef char* STRING;
+
+typedef enum { Info, Upozorenje, Greska } STATUS;
+
+typedef struct poruka {
+	struct {
+		STRING kreiraj, unisti, ubaci, izbaci, sortiraj;
+	} GRESKA;
+	struct {
+		STRING ubaci, izbaci, sortiraj;
+	} UPOZORENJE;
+	struct {
+		STRING kreiraj, unisti, ubaci, izbaci, podatak_postoji, podatak_ne_postoji, lista_prazna, lista_nije_prazna, sortiraj;
+	} INFO;
+} PORUKA;
+
+typedef struct signal {
+	STATUS status;
+	STRING poruka;
+} SIGNAL;
+
+// funkcije
+void obrada_statusa(STATUS, STRING, STRING, int); ///////////
